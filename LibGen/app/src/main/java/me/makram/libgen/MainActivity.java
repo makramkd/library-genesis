1ac7a68549da00a3114853a61f61fedc54bdcb6b
Makram Kamaleddine <makram.kd@gmail.com>
2015-09-06 18:14:29 +0400

    Adding some skeleton code and the permissions in the manifest. This is basically what the application code would look like, except that we have to keep track of what page we’re on.

--- LibGen/app/src/main/java/me/makram/libgen/MainActivity.java
+++ LibGen/app/src/main/java/me/makram/libgen/MainActivity.java
@@ -1,16 +1,43 @@
 package me.makram.libgen;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final String SEARCH_URL =
+            "http://gen.lib.rus.ec/search.php?&req=%s&phrase=1&view=simple&column=def&sort=title&sortmode=ASC&page=%d";
+
+    private final OkHttpClient client = new OkHttpClient();
+
+    EditText searchEdit;
+    Spinner sourceSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchEdit = (EditText) findViewById(R.id.searchEdit);
+        sourceSpinner = (Spinner) findViewById(R.id.searchSourceSpinner);
+
     }
 
     @Override
@@ -33,5 +60,52 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void searchButtonClicked(View view) {
+        String query = searchEdit.getText().toString();
+        int pageNumber = 1;
+        String finalUrl = String.format(SEARCH_URL, query, pageNumber);
+
+        Request request = new Request.Builder()
+                .url(finalUrl)
+                .build();
+
+        new GetPage().execute(request);
+    }
+
+    private class GetPage extends AsyncTask<Request, Void, Void> {
+
+        boolean succeeded = false;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            Toast.makeText(getApplicationContext(), "Downloading page", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected Void doInBackground(Request... params) {
+            try {
+                Response response = client.newCall(params[0]).execute();
+                Document document = Jsoup.parse(response.body().string());
+                succeeded = true;
+            } catch (Exception e) {
+                Log.d("MA", "Exception in AsyncTask#GetPage: " + e.getMessage());
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            if (succeeded) {
+                Toast.makeText(getApplicationContext(), "Finished downloading and parsing",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
