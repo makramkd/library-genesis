f5337edf7a81a4eb9aea2d7f9245921caeb1a7ec
Makram Kamaleddine <makram.kd@gmail.com>
2015-09-07 12:40:35 +0400

    Adding the endless scroll listener which automatically updates the data set whenever we get close to the end.

---
+++ LibGen/app/src/main/java/me/makram/libgen/EndlessScrollListener.java
@@ -0,0 +1,46 @@
+package me.makram.libgen;
+
+import android.content.Context;
+import android.widget.AbsListView;
+
+/**
+ * Created by admin on 9/7/15.
+ */
+public class EndlessScrollListener implements AbsListView.OnScrollListener {
+
+    private int visibleThreshold;
+    private int currentPage;
+    private int previousTotal;
+    private boolean loading;
+    private Context context;
+    private EntryAdapter entryAdapter;
+
+    public EndlessScrollListener(Context context, EntryAdapter adapter, int visibleThreshold) {
+        this.visibleThreshold = visibleThreshold;
+        currentPage = 1;
+        previousTotal = 0;
+        loading = true;
+        this.context = context;
+        this.entryAdapter = adapter;
+    }
+
+    @Override
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
+    }
+
+    @Override
+    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+        if (loading) {
+            if (totalItemCount > previousTotal) {
+                loading = false;
+                previousTotal = totalItemCount;
+                currentPage++;
+            }
+        }
+        if (!loading && (totalItemCount - visibleItemCount) <= (firstVisibleItem + visibleThreshold)) {
+            // use async task to load more entries
+            new LoadMoreEntriesTask(context, entryAdapter).execute();
+            loading = true;
+        }
+    }
+}
